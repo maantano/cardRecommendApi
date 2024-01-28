68dffc714080def82b043db06b86a4031309909f
maantano <maantano@naver.com>
2024-01-29 03:30:33 +0900

    .env

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.listen(PORT, () => {
   console.log("process.env.DB_USER ==>", process.env.DB_USER);
   console.log("process.env.PASSWORD ==>", process.env.DB_PASSWORD);
   console.log("process.env.DB_NAME ==>", process.env.DB_NAME);
-  console.log("process.env.DB_PORT ==>", process.env.DB_PORT);
+  // console.log("process.env.DB_PORT ==>", process.env.DB_PORT);
   console.log("process.env.DB_HOST ==>", " ===================== ");
 });
 
