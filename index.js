0441cce777c20ff1736acd39e1c733c13185dd09
maantano <maantano@naver.com>
2024-01-31 02:55:55 +0900

    ip

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ const app = express();
 // ================================================================================================
 const PORT = process.env.port || 9000;
 const csv = require("csv-parser");
+const IP = "0.0.0.0";
 require("dotenv").config();
 
 const fs = require("fs");
